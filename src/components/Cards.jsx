b36d4be9624a4330ea805b703a214b98b47199db
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2022-01-14 22:57:35 +0300

    start work with cards

---
+++ src/components/Cards.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Wrapper = styled.article``
+
+const CardImage = styled.img``
+
+const CardBody = styled.div``
+
+const CardTitle = styled.h3``
+
+const CardList = styled.ul``
+
+const CardListItem = styled.li``
+
+const Card = () => {
+    return (  );
+}
+
+export default Card;
